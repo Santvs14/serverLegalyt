c3fa7fb24424c8c4db17cf9224843674bb2c18a6
SantiagoVS <santiagovs1402@gmail.com>
2024-11-14 23:21:35 -1100

    continuar corrigiendo errores Ies

--- routes/iesRoutes.js
+++ routes/iesRoutes.js
@@ -1,15 +1,12 @@
+// En tu archivo de rutas
 const express = require('express');
 const router = express.Router();
 const iesController = require('../controllers/iesController');
 
-// Definir la ruta para crear un nuevo registro IES
-router.post('/api/ies', iesController.createIES);
+// Ruta para crear un nuevo registro IES con archivos
+router.post('/api/ies', iesController.uploadDocuments, iesController.createIES);
 
-
-// Ruta para obtener todos los registros IES
-router.get('/ies', iesController.getAllIES);
-
-// Ruta para agregar un nuevo documento a un registro IES
-router.put('/:id/documento', iesController.uploadDocuments, iesController.addDocument);
+// Ruta para agregar documentos a un registro IES existente
+router.put('/api/ies/:id/documento', iesController.uploadDocuments, iesController.addDocument);
 
 module.exports = router;
