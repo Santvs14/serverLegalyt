688106510f667d8d6a7458443027c027ca683445
SantiagoVS <santiagovs1402@gmail.com>
2024-11-14 22:36:26 -1100

    corregir errores de  la Ies

--- routes/iesRoutes.js
+++ routes/iesRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const iesController = require('../controllers/iesController');
 
 // Ruta para crear un nuevo registro IES con documentos
-router.post('/ies', iesController.uploadDocuments, iesController.createIES);
+router.post('/', iesController.createIES);
 
 // Ruta para obtener todos los registros IES
 router.get('/ies', iesController.getAllIES);
 
 // Ruta para agregar un nuevo documento a un registro IES
-router.post('/ies/:id/documento', iesController.addDocument);
+router.put('/:id/documento', iesController.uploadDocuments, iesController.addDocument);
 
 module.exports = router;
