b0a125027e8e359be3cacfbb85e100bb2e6d7930
SantiagoVS <santiagovs1402@gmail.com>
2024-11-15 17:11:33 -1100

    Probando cambio en routes

--- routes/iesRoutes.js
+++ routes/iesRoutes.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const multer = require('multer');
 const iesController = require('../controllers/iesController');
 
-// Configuración de Multer para manejar archivos en el backend
-const storage = multer.diskStorage({});
+// Configuración de Multer para manejar archivos
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Asigna el directorio donde se almacenarán los archivos
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
 const upload = multer({ storage });
 
 // Ruta para crear un nuevo registro de IES
-router.post('/create', iesController.createIES);
+router.post('/create', upload.array('documentos'), iesController.createIES); // Asegúrate de usar el middleware de multer aquí
 
 module.exports = router;
