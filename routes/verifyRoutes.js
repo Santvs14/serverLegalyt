2ec52d6ce6690ba1341487cfc76f54308e0cede8
SantiagoVS <santiagovs1402@gmail.com>
2024-11-25 01:23:37 -1100

    Nuevos cambios 3

--- routes/verifyRoutes.js
+++ routes/verifyRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { sendCode, verifyUser } = require('../controllers/userController');
+const sendVerificationEmail = require('../utils/sendVerifyEmail');  // Importa la función
+
 
 // Ruta para enviar el código de verificación (email en este caso)
-router.post('/send-code', sendCode);
+// En el backend
+app.post('/send-code', async (req, res) => {
+    try {
+        const { email } = req.body;
+        const result = await sendVerificationEmail(email);
+        console.log('Resultado:', result); // Agrega log para verificar los datos
+        res.status(200).json(result);
+    } catch (error) {
+        console.error('Error al enviar código:', error);
+        res.status(500).json({ message: 'Error al enviar el código' });
+    }
+});
+
 
 // Ruta para verificar el código
 router.post('/verify-code', verifyUser);
